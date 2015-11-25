ac0e6af4886b8b3f1ae076cb3387b4f253ebf504
Benjamin Bouachour <benjaminbouachour@iMac-de-Benjamin.local>
2015-11-25 19:06:38 +0100

    add compression tools

---
+++ filetools.go
@@ -0,0 +1,30 @@
+package tools
+
+import (
+	"compress/gzip"
+	"errors"
+	"io"
+	"os"
+)
+
+// later on integrate https://github.com/youtube/vitess/blob/master/go/cgzip/
+
+var compresserror error = errors.New("Unable to generate a compressor")
+
+//CompressFiles gzip files into result
+func CompressFiles(files []string, result io.Writer) (int, error) {
+	succeed := 0
+	zipper := gzip.NewWriter(result)
+	for _, filepath := range files {
+		if file, errOpen := os.OpenFile(filepath, os.O_RDONLY, 0755); errOpen != nil {
+			continue
+		} else {
+
+			if _, errCp := io.Copy(zipper, file); errCp == nil {
+				succeed++
+			}
+			file.Close()
+		}
+	}
+	return succeed, nil
+}
