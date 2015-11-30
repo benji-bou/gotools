341bd68bbc1c241838a3b47dc3b71da1c98c8667
Benjamin Bouachour <benjamin.bouachour@bioserenity.com>
2015-11-30 08:33:50 +0100

    repair archive multiple files

--- filetools.go
+++ filetools.go
@@ -1,7 +1,7 @@
 package tools
 
 import (
-	"compress/gzip"
+	"archive/zip"
 	"errors"
 	"io"
 	"log"
@@ -10,23 +10,49 @@ import (
 
 // later on integrate https://github.com/youtube/vitess/blob/master/go/cgzip/
 
+func init() {
+	// see https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT
+	// section 4.4.5 for the available compression methods...which are not implemented by Golang....
+	// only algorithm 0 and number 8(deflate) available
+	//
+}
+
 var compresserror error = errors.New("Unable to generate a compressor")
 
 //CompressFiles gzip files into result
 func CompressFiles(files []string, result io.Writer) (int, error) {
 	succeed := 0
-	zipper := gzip.NewWriter(result)
+	archive := zip.NewWriter(result)
+	defer archive.Close()
+
 	for _, filepath := range files {
 		if file, errOpen := os.OpenFile(filepath, os.O_RDONLY, 0755); errOpen != nil {
 			log.Println(errOpen)
 			continue
 		} else {
+			defer file.Close()
+			info, errStat := file.Stat()
+			if errStat != nil {
+				continue
+			}
+			header, err := zip.FileInfoHeader(info)
+			if err != nil {
+				continue
+			}
+
+			header.Method = zip.Deflate
+
+			zipper, err := archive.CreateHeader(header)
+			if err != nil {
+				continue
+			}
+
 			if _, errCp := io.Copy(zipper, file); errCp == nil {
 				succeed++
+			} else {
+				log.Println(errCp)
 			}
-			file.Close()
 		}
 	}
-	zipper.Flush()
 	return succeed, nil
 }
